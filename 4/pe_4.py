d0a038576fb4469784c96621012e26adcc96f6f8
Christian <christian@strandkarr.com>
2021-12-14 23:14:20 +0100

    Part 4. Added timing and another (slower version)

--- 4/pe_4.py
+++ 4/pe_4.py
@@ -1,3 +1,5 @@
+import time
+
 
 def is_palindrome(number):
     str_num = str(number)
@@ -7,6 +9,15 @@ def is_palindrome(number):
 def solve(max_limit):
     max_palindrome = 0
     for i in range(max_limit, max_limit // 10, -1):
+        for j in range(max_limit, i - 1, -1):
+            if is_palindrome(i * j) and (i * j) > max_palindrome:
+                max_palindrome = i * j
+    return max_palindrome
+
+
+def solve2(max_limit):
+    max_palindrome = 0
+    for i in range(max_limit, max_limit // 10, -1):
         for j in range(max_limit, max_limit // 10, -1):
             if is_palindrome(i * j) and (i * j) > max_palindrome:
                 max_palindrome = i * j
@@ -14,7 +25,14 @@ def solve(max_limit):
 
 
 def main():
+    t1 = time.perf_counter()
     print(f"{solve(999)=}")
+    t2 = time.perf_counter()
+    print(f"{t2-t1=}")
+    t1 = time.perf_counter()
+    print(f"{solve2(999)=}")
+    t2 = time.perf_counter()
+    print(f"{t2-t1=}")
 
 
 def test():
